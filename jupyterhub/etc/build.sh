9ceede0845d89899e264f256a95461880d93f9ad
Rodrigo Alvares <ralvares@redhat.com>
2021-11-03 11:27:42 +0400

    First Commit

---
+++ jupyterhub/etc/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#pip3 install -U pip setuptools wheel
+
+# Install the required application packages.
+
+#pip3 install jmespath openshift ansible setuptools-rust jupyterhub jupyterhub-tmpauthenticator jupyterhub-kubespawner oauthenticator wrapt
+
+# Create data directory for database files.
+
+#mkdir /opt/app-root/data
